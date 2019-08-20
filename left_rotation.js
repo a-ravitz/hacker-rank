11b0d25e524635a2cec3e6cdd4557d656eacc3e1
a-ravitz <adam.ravitz@gmail.com>
2019-08-20 00:11:49 -0400

    minimum_swaps

--- left_rotation.js
+++ left_rotation.js
@@ -1,4 +1,6 @@
-//
+// A left rotation operation on an array shifts each of the array's elements  unit to the left. 
+// For example, if 2 left rotations are performed on array [1,2,3,4,5], then the array would become [3,4,5,1,2].
+// Given an array a of n integers and a number, d, perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
 
 let arr = [1, 2, 3, 4, 5, 6];
 let d = 3;
