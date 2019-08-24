546aba6ccc3206a9e22ddf6643f5401d49b195c3
a-ravitz <adam.ravitz@gmail.com>
2019-08-24 11:43:29 -0400

    array manipulation solutions

--- array_manipulation.js
+++ array_manipulation.js
@@ -1,25 +1,67 @@
+
 let n = 5;
 let queries = [[1, 2, 100], [2, 5, 100], [3, 4, 100]];
-// let queries = [[1, 2, 100]];
+// //solution without prefix sum
 
+// function arrayManipulation(n, queries) {
+
+//     findMax  = (arr) => {
+//         console.log('infindmax')
+//         let max = Math.max(...arr)
+//         console.log('max:', max)
+//         console.log(max)
+//     }
+
+//     const len = queries.length
+//     let arr = [];
+
+//   for (let i = 0; i < n; i++) {
+//     arr.push(0);
+//   }
+//     for (let j = 0; j < len; j++) {
+
+//     let b = queries[j][1] - 1
+//     let k = queries[j][2]
+//         for(let a = queries[j][0] - 1; a <= b; a++ ) {
+//             arr[a] += k
+//             }
+//         }
+//         findMax(arr)
+//     }
+
+// console.log(arrayManipulation(n, queries))
+
+// // solution with prefix sum
 function arrayManipulation(n, queries) {
-
-  const len = queries.length
+  const len = queries.length;
   let arr = [];
+  let max = 0;
 
-  for (let i = 0; i < n; i++) {
+  //making the 0's
+  for (let j = 0; j < n; j++) {
     arr.push(0);
   }
-    for (let j = 0; j < len; j++) {
-        
-        let b = queries[j][1] - 1
-        let k = queries[j][2]
-            for(let a = queries[j][0] - 1; a <= b; a++ ) {
-            arr[a] += k
-            }
-    }
 
-    return Math.max(...arr)
+  for (let i = 0; i < len; i++) {
+    /*a = queries[i][0] - 1;
+      b = queries[i][1];
+      k = queries[i][2]; */
+
+    arr[queries[i][0] - 1] += queries[i][2];
+    if (queries[i][1] < arr.length) {
+      arr[queries[i][1]] -= queries[i][2];
+    }
+  }
+
+  for (let a = 1; a < n; a++) {
+    arr[a] += arr[a - 1];
+  }
+
+  for (let k = 0; k < arr.length; k++) {
+    max = Math.max(max, arr[k]);
+  }
+
+  return max;
 }
 
-console.log(arrayManipulation(n, queries))
+console.log(arrayManipulation(n, queries));
