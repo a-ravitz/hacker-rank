7cfd7b1cb78e821fd4b6af04ac19fce276f8720c
a-ravitz <adam.ravitz@gmail.com>
2019-09-05 11:57:33 -0400

    regex guide updated

--- 10 days of JavaScript/day_7_regular_expressions_i.js
+++ 10 days of JavaScript/day_7_regular_expressions_i.js
@@ -1,12 +1,16 @@
 function regexVar() {
-    
-    // /'s begin and end the statment
-    // ^ denotes the first character
-    // ([contents]) ~ the contents are characters that we're trying to match for the beginning
-    // .* stipulates that the captured character must followed by zero or more occurrences of any character.
-    /* \1 is a backreference to the first capture group. It stipulates that the character following whatever we matched in step 2 must match whatever 
-    was captured inside the parentheses in step 1. */
-    // $ Matches end of input
+
+    // /'s => begin and end the statment
+    // ^ => denotes first item matches
+    // () => stores matching value captured within
+    // [] => matches any of the characters within brackets
+    // . => matches any character 
+    // + => for 1 or more occurrances (this ensures str length > 3)
+    // .* => stipulates that the captured character must followed by zero or more occurrences of any character.
+    // \1 => matches to previously stored match. 
+        // \2 looks for matched item stored 2 instances ago 
+        // \3 looks for matched item stored 3 ago, etc
+    //  $ => ensures that matched item is at end of the sequence
 
     const re = /^([aeiou]).*\1$/
     return re;
@@ -14,7 +18,7 @@ function regexVar() {
 
 function main() {
     const re = regexVar();
-    const s = 'abcdiuidsv'
+    const s = 'abcdi  uidsa'
     
     console.log(re.test(s));
 }
