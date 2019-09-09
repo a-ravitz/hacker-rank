d372b4992f5cdb5f1076a3670650d64491a70253
a-ravitz <adam.ravitz@gmail.com>
2019-09-09 11:01:11 -0400

    changed apples and oragnes

--- apples_and_oranges.js
+++ apples_and_oranges.js
@@ -1,18 +1,19 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
-let apple = 0
-let orange = 0 
+let aCount = 0
+let oCount = 0 
 
     for (let i = 0; i < apples.length; i++) {
         if (apples[i] + a >= s && apples[i] + a <= t ) {
-            apple ++
+            aCount ++
         }
     }
     for (let j = 0; j < oranges.length; j++) {
         if(oranges[j] + b >= s && oranges[j] + b <= t) {
-            orange ++
+            oCount ++
         }
     }
-    return apple + '\n'+ orange
+    console.log(aCount) + '\n'+ 
+    console.log(oCount)
 }
 
-console.log(countApplesAndOranges(7, 11, 5, 15, [-2,2,1],[5,-6]))
+countApplesAndOranges(7, 11, 5, 15, [-2,2,1],[5,-6])
