2844d3d9b1032fa446be1f74b7888b5058be7ee0
a-ravitz <adam.ravitz@gmail.com>
2019-09-09 13:43:11 -0400

    super reduced string

---
+++ super_reduced_string.js
@@ -0,0 +1,21 @@
+function superReducedString(s) {
+
+  let str = s;
+
+  for (let i = 1; i < str.length; i++) {
+    if (str.charAt(i) === str.charAt(i - 1)) {
+      str = str.substring(0, i - 1) + str.substring(i + 1);
+      i = 0;
+    }
+  }
+  
+  if (str.length === 0) {
+    return "Empty String"
+  } else {
+    return str
+  }
+
+}
+console.log(superReducedString("aaabccddd"));
+console.log(superReducedString("mwkommokwmxjivkkvijxshscbbcshsgwdyqqydwgzpnlzzlnpzvfeaiiaefvyeqjccjqeymhqhiihqhmhaomkkmoahhddymmyddh"));
+console.log(superReducedString("baab"));
