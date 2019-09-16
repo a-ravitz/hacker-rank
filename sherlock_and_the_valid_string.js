a4668d91c255f96b6b9068a1097ee9dc7ad5a676
a-ravitz <adam.ravitz@gmail.com>
2019-09-16 11:32:21 -0400

    sherlock and the valid string

---
+++ sherlock_and_the_valid_string.js
@@ -0,0 +1,35 @@
+function isValid(s) {
+
+    let validator = {}
+   
+    //add items to map
+    for (let i = 0; i < s.length; i++) {
+        if (!validator[s[i]]) {
+            validator[s[i]] = 1
+        } else {
+            validator[s[i]] ++
+        }
+    }
+    
+    let toBeSorted = []
+    for (let values in validator) {
+        toBeSorted.push([values, validator[values]])
+    }
+    //push items to array, sort based on value
+    let sorted = toBeSorted.sort((a,b) => a[1] - b[1])
+
+    //conditionals 
+    if (sorted[0][1] === sorted[sorted.length-1][1]) {
+        return 'YES'
+    } else if (sorted[0][1] === sorted[sorted.length-2][1] && sorted[0][1] === sorted[sorted.length-1][1]-1){
+        return 'YES'
+    } else if (sorted[1][1] === sorted[sorted.length-1][1]) {
+        return 'YES'
+    } else {
+        return 'NO'
+    }
+    
+}
+
+console.log(isValid('ibfdgaeadiaefgbhbdghhhbgdfgeiccbiehhfcggchgghadhdhagfbahhddgghbdehidbibaeaagaeeigffcebfbaieggabcfbiiedcabfihchdfabifahcbhagccbdfifhghcadfiadeeaheeddddiecaicbgigccageicehfdhdgafaddhffadigfhhcaedcedecafeacbdacgfgfeeibgaiffdehigebhhehiaahfidibccdcdagifgaihacihadecgifihbebffebdfbchbgigeccahgihbcbcaggebaaafgfedbfgagfediddghdgbgehhhifhgcedechahidcbchebheihaadbbbiaiccededchdagfhccfdefigfibifabeiaccghcegfbcghaefifbachebaacbhbfgfddeceababbacgffbagidebeadfihaefefegbghgddbbgddeehgfbhafbccidebgehifafgbghafacgfdccgifdcbbbidfifhdaibgigebigaedeaaiadegfefbhacgddhchgcbgcaeaieiegiffchbgbebgbehbbfcebciiagacaiechdigbgbghefcahgbhfibhedaeeiffebdiabcifgccdefabccdghehfibfiifdaicfedagahhdcbhbicdgibgcedieihcichadgchgbdcdagaihebbabhibcihicadgadfcihdheefbhffiageddhgahaidfdhhdbgciiaciegchiiebfbcbhaeagccfhbfhaddagnfieihghfbaggiffbbfbecgaiiidccdceadbbdfgigibgcgchafccdchgifdeieicbaididhfcfdedbhaadedfageigfdehgcdaecaebebebfcieaecfagfdieaefdiedbcadchabhebgehiidfcgahcdhcdhgchhiiheffiifeegcfdgbdeffhgeghdfhbfbifgidcafbfcd'))
+
