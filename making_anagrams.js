a71916db4bbd82056e452478079e9391813ef275
a-ravitz <adam.ravitz@gmail.com>
2019-09-16 12:30:33 -0400

    updated making anagrams

--- making_anagrams.js
+++ making_anagrams.js
@@ -2,7 +2,7 @@ function makeAnagram(a, b) {
   let string1 = {};
   let string2 = {};
   let count = 0;
-  
+
   for (let i = 0; i < a.length; i++) {
     if (b.includes(a[i])) {
       if (!string1[a[i]]) {
@@ -21,33 +21,22 @@ function makeAnagram(a, b) {
       }
     }
   }
-  
-  let sortedOne = Object.keys(string1).sort().reduce((a, b) => {
-    a[b] = string1[b];
-    return a; 
-  }, {});
-  let sortedTwo = Object.keys(string2).sort().reduce((a, b) => {
-    a[b] = string2[b];
-    return a;
-  }, {});
 
-  for (const value in sortedTwo) {
-    if (sortedTwo[value] > sortedOne[value]) {
-      count += (sortedOne[value] * 2)
-    } 
-    if (sortedTwo[value] === sortedOne[value]) {
-      count += sortedTwo[value] + sortedOne[value]
-    }  
-    if (sortedTwo[value] < sortedOne[value]) {
-      count += (sortedTwo[value] * 2)
-
-    }
-    
+  for (const value in string1) {
+      if (string1[value] > string2[value]) {
+        count += string2[value] * 2;
+      }
+      if (string1[value] === string2[value]) {
+        count += string1[value] + string2[value];
+      }
+      if (string1[value] < string2[value]) {
+        count += string1[value] * 2;
+      }
   }
-  return (a.length + b.length) - count 
-
+  return a.length + b.length - count;
 }
-//
+
 // console.log(makeAnagram("cde", "abc"));
-// console.log(makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'))
-console.log(makeAnagram("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss", "tttttttttttttttttttttttttttttttttttttsssssssssssssssss"));
+console.log(makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'))
+console.log(makeAnagram("sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss",
+      "tttttttttttttttttttttttttttttttttttttsssssssssssssssss"));
