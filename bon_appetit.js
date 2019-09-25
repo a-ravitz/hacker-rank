2d6cffaf38cc7533fc0582e573018a98cbbe1faf
a-ravitz <adam.ravitz@gmail.com>
2019-09-25 19:51:24 -0400

    bon appetit

---
+++ bon_appetit.js
@@ -0,0 +1,19 @@
+let bill = [ 3, 10, 2, 9 ] 
+let index = 1 
+let annasPrice = 12
+
+
+
+function bonAppetit(bill, k, b) {
+
+    let annasShare = bill.reduce((a, b) => a + b) - bill[k]
+    let overPaid = 'Bon Appetit'
+
+    if(b - (annasShare / 2) !== 0) {
+        overPaid = b - (annasShare/2);
+    }
+
+    console.log(overPaid) 
+
+}
+bonAppetit(bill, index, annasPrice)
