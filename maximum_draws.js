667c6159675209db8a45d4712a6ef4a5861cf77a
a-ravitz <adam.ravitz@gmail.com>
2019-09-26 13:39:34 -0400

    maximum_draws

---
+++ maximum_draws.js
@@ -0,0 +1,21 @@
+/* n pairs of socks are present in the drawer, but are not bound. what is the maximum 
+number of lose socks you'll have to pull from the drawer to get a match
+
+~2 pairs~
+1 1
+2
+
+~4 pairs~ 
+1 1
+2
+3
+4
+
+*/
+
+function maximumDraws(n) {
+
+    return n+1
+}
+
+maximumDraws(2)
