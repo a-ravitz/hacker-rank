ed369717d700332b85d043543c378edd457d64a3
a-ravitz <adam.ravitz@gmail.com>
2019-09-27 23:16:11 -0400

    slight change

--- leonardos_prime_factors.js
+++ leonardos_prime_factors.js
@@ -1,7 +1,7 @@
 function primeCount(n) {
-    let primeArr = [3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
-    let count = 1;
-    let total = 2;
+    let primeArr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    let count = 0;
+    let total = 1;
   
     if (n === 1) {
       return 0;
@@ -18,7 +18,17 @@ function primeCount(n) {
   }
 
 console.log(primeCount(10000000000))
-
+console.log(primeCount(614889782588491410));
+console.log(primeCount(614889782588491409));
+// console.log(primeCount(614889782588491408));
+// console.log(primeCount(614889782588491407));
+// console.log(primeCount(6148897825884914));
+// console.log(primeCount(614889782588));
+// console.log(primeCount(614889782588491410));
+// console.log(primeCount(614889782588491411));
+// console.log(primeCount(614889782588491412));
+// console.log(primeCount(614889782588491413));
+// console.log(primeCount(614889782588491415));
 
 
 
