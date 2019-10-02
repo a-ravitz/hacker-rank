c385847b8c64f22f70d50db83bf80865175b67df
a-ravitz <adam.ravitz@gmail.com>
2019-10-02 15:25:24 -0400

    better solution special string

--- special_string_again.js
+++ special_string_again.js
@@ -1,8 +1,10 @@
 function substrCount(n, s) {
-  let list = [], temp = 0, count = 0;
+  let list = [],
+    temp = 0,
+    count = 0;
 
   // condense the string into a list of [characters,numbers]
-  //if the string is aaabccbbccba our list will look like 
+  //if the string is aaabccbbccba our list will look like
   //  [[a,3],[b,1][c,2][b,2][c,2][b,1][c,1]]
 
   for (let i = 0; i < s.length; i++) {
@@ -17,31 +19,28 @@ function substrCount(n, s) {
   }
 
   //if list[i][1] === 4, like in the case of 'aaaa'
-  //we know that there are ten possible matches 
+  //we know that there are ten possible matches
   //which is the equivelent of 4 * 5 / 2
-  //so we add that to the count 
+  //so we add that to the count
   for (let i = 0; i < list.length; i++) {
     count += (list[i][1] * (list[i][1] + 1)) / 2;
-  }
 
-  //if the list length is greater than or eql to 3
-  //if [i-1][0] === [i+1][0], and [i][1] === 1
-  //we have a pattern like aaaaafaaaa
+    //if the list length is greater than or eql to 3
+    //if [i-1][0] === [i+1][0], and [i][1] === 1
+    //we have a pattern like aaaaafaaaa
 
-  //if that is true, we take the smaller number of a's
-  //Math.min(list[i - 1][1], list[i+1][1])
-  //and add it to our count 
-  
-  if (list.length >= 3) {
-    for (let i = 1; i < list.length -1; i++) {
-      if (list[i - 1][0] === list[i+1][0] && list[i][1] === 1) {
-        count += Math.min(list[i - 1][1], list[i+1][1]);
+    //if that is true, we take the smaller number of a's
+    //Math.min(list[i - 1][1], list[i+1][1])
+    //and add it to our count
+
+    if (list.length >= 3) {
+      if (list[i + 1] && list[i - 1]) {
+        if (list[i - 1][0] === list[i + 1][0] && list[i][1] === 1) {
+          count += Math.min(list[i - 1][1], list[i + 1][1]);
+        }
       }
     }
   }
-
   return count;
-
 }
 console.log(substrCount(4, "aaaa"));
-
