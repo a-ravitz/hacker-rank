25252ffb47b7d99bdd42dedfa3149df38c8882d2
a-ravitz <adam.ravitz@gmail.com>
2019-10-03 16:27:20 -0400

    birthday cake candles update

--- birthday_cake_candles.js
+++ birthday_cake_candles.js
@@ -1,10 +1,11 @@
 function birthdayCakeCandles(ar) {
-    
-    let max = ar.reduce((a,b) => Math.max(a, b))
 
-    let tallCandles =  ar.filter((c) => c === max).length
+    let max = Math.max(...ar)
+
+    let tallCandles =  ar.filter((candle) => candle === max).length
 
     return tallCandles
+    
 }
 
 console.log(birthdayCakeCandles([18, 90, 90, 13, 90, 75, 90, 8, 90, 43]))
