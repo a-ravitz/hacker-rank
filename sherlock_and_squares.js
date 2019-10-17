f1e297e4521c0da594c3aa08de691a11656f2512
a-ravitz <adam.ravitz@gmail.com>
2019-10-17 09:32:10 -0400

    squares

--- sherlock_and_squares.js
+++ sherlock_and_squares.js
@@ -1,19 +1,9 @@
 function squares(a, b) {
 
-    /* if we run code that looks like this 
-    let count = 0
-    for (let i = a; i <= b; i++) {
-        if (Math.sqrt(i) % 1 === 0) {
-            count ++
-        }
-    }
-    return count
-
-    the program will time out
-
-    its more effecient to find Math.sqrt(a) and round that up & Math.sqrt(b) 
-    and round that down, then if A is smaller than B subtract B from A and add 1 
-    */
+    /* given the range from 30 to 70 we could check all 40 numbers to see if they're squares
+    or we could get the sqrt of 30 and round it up to the next whole number, 6, and get 
+    the sqrt of 70 and round it down to the last whole number, 8, then subtract 6 from 8 and 
+    add 1.  The answer would be 3 => 36, 49, and 64. */
 
 
   let sqrtA = Math.ceil(Math.sqrt(a));
@@ -25,3 +15,4 @@ function squares(a, b) {
 console.log(squares(10, 30));
 console.log(squares(17, 24));
 console.log(squares(385793959, 712365911));
+console.log(Math.sqrt(70))
