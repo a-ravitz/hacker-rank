85960034280482e9b4ba6e2ee020c27a6e5079d9
a-ravitz <adam.ravitz@gmail.com>
2019-12-14 18:44:06 -0500

    formatting

--- insertion_sort_-_part_1.js
+++ insertion_sort_-_part_1.js
@@ -4,12 +4,12 @@ function insertionSort1(n, arr) {
 
   for (let i = n; i >= 0; i--) {
     if (last < arr[i - 1]) {
-      arr[i] = arr[i - 1];
-      console.log(arr.join(" "));
+        arr[i] = arr[i - 1];
+        console.log(arr.join(" "));
     } else if (last > arr[i]) {
-      arr[i + 1] = last;
-      console.log(arr.join(" "));
-      break;
+        arr[i + 1] = last;
+        console.log(arr.join(" "));
+        break;
     } else if (arr[0] === arr[1]) {
         arr[0] = last
         console.log(arr.join(" "));
