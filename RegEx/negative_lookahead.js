29235e6a5e5995d8442e9656a090223f8b836f80
a-ravitz <adam.ravitz@gmail.com>
2020-07-07 22:45:33 -0400

    negative lookahead

--- RegEx/negative_lookahead.js
+++ RegEx/negative_lookahead.js
@@ -1,11 +1,11 @@
 function processData(str) {
     //Enter your code here
-    let Array = str.match(/o(?=oo)/g);
+    let Array = str.match(/(.)(?!\1)/g);
     console.log("Number of matches :", Array.length);
 } 
 
-//expect number of matches to be 5
-processData('oooooogooo')
+//expect number of matches to be 2
+processData('gooooo')
 
 //expect number of matches to be 3 
 processData('gooooo!')
